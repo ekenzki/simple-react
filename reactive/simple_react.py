264336b29b28e308276d76f9f9992ac48933c200
Konstantinos Tsakalozos <kos.tsakalozos@canonical.com>
2018-05-31 14:28:13 +0300

    Add the scania train interface

--- reactive/simple_react.py
+++ reactive/simple_react.py
@@ -11,32 +11,11 @@ from charmhelpers.core import hookenv
 
 @when_not('simple-react.installed')
 def install_simple_react():
-    # Do your setup here.
-    #
-    # If your charm has other dependencies before it can install,
-    # add those as @when() clauses above., or as additional @when()
-    # decorated handlers below
-    #
-    # See the following for information about reactive charms:
-    #
-    #  * https://jujucharms.com/docs/devel/developer-getting-started
-    #  * https://github.com/juju-solutions/layer-basic#overview
-    #
     log("Installing git")
     status_set("waiting", "Installing git")
     charms.apt.queue_install(['git'])
-    log("Git installed")
+    log("Git is about to be installed")
     set_state('simple-react.installed')
-
-@when_not('all_done')
-@when_all('apt.installed.git', 'db.available')
-def installed():
-    mysql = endpoint_from_flag('db.available')
-    log(mysql.connection_string())
-
-    log("All done!")
-    status_set("active", "Ready")
-    set_state('all_done')
 
 @when_not('db.connected')
 def wating_db():
@@ -49,4 +28,18 @@ def starting_db():
     log("db connected but not available")
     status_set("waiting", "Waiting for mysql to start")
 
+@when_not('all_done')
+@when_all('apt.installed.git', 'db.available')
+def installed():
+    mysql = endpoint_from_flag('db.available')
+    log(mysql.connection_string())
 
+    log("All done!")
+    status_set("active", "Ready - accepting connections")
+    set_state('all_done')
+
+@when_all('frontend.scania.present', 'all_done')
+def signal_ready():
+    log("Sending ready signal to frontends")
+    frontend = endpoint_from_flag('frontend.scania.present')
+    frontend.ready()
